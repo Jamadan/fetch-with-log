654ead115268b2e848c473f8fb573ab341daa0d8
Jamie Hartley <jamie.hartley1@tesco.com>
2019-03-27 20:32:29 +0000

    added test harness, figured out babel! (WOOP)

--- test-harness.js
+++ test-harness.js
@@ -1,22 +1,34 @@
 import fetchWithLog from "./src";
+// OR
+// const fetchWithLog = require('fetch-with-log').default;
 
 console.log("BEGIN");
 console.log("--------");
 
 const log = args => {
-  console.log("Passed func");
-  console.log(args);
+  // do logging magic here
+  // args are this shape:
+  // {
+  //   url, // url requested
+  //   options, // request options passed
+  //   res, // response object
+  //   time; // time taken to make fetch request
+  // }
+
+  // UNCOMMENT HERE TO PRINT ARGS
+  // console.log(args);
+
+  console.log("Passed log function");
 };
 
-const fetch = new fetchWithLog({ log: log });
+const fetch = fetchWithLog({ log });
 
 (async function init() {
   const result = await fetch("https://www.google.com", {}).catch(e => {
     console.log("ERROR");
-
     console.log(e);
   });
-  console.log(result);
+  // console.log(result);
 
   console.log("COMPLETE");
   console.log("--------");
