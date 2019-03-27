7e5418da41f6f84acb9607cc23d73f38148caa30
Jamie Hartley <jamie.hartley1@tesco.com>
2019-03-27 22:24:02 +0000

    more testing

--- src/main/index.test.js
+++ src/main/index.test.js
@@ -1,5 +1,7 @@
 import fetchWithLog from './';
 
+const originalFetch = fetch;
+
 describe('fetchWithLog', () => {
   it('returns function', () => {
     const testLog = () => {};
@@ -16,5 +18,68 @@ describe('fetchWithLog', () => {
         new TypeError("Cannot read property 'log' of undefined")
       );
     }
+  });
+
+  describe('fetch', () => {
+    it('returns same as fetch', async () => {
+      const testLog = () => {};
+
+      const fetch = fetchWithLog({ log: testLog });
+      let fetchResult = await originalFetch('https://www.google.com');
+      let fetchWithLogResult = await fetch('https://www.google.com');
+
+      expect(fetchResult.status).toEqual(fetchWithLogResult.status);
+
+      let fetchError;
+      let fetWithLogError;
+      try {
+        fetchResult = await originalFetch('www.google.com');
+      } catch (ex) {
+        fetchError = ex;
+      }
+
+      try {
+        fetchWithLogResult = await fetch('www.google.com');
+      } catch (ex) {
+        fetWithLogError = ex;
+      }
+
+      expect(fetchError).toEqual(fetWithLogError);
+    });
+
+    it('logs to function', async () => {
+      const testLog = jest.fn();
+
+      const fetch = fetchWithLog({ log: testLog });
+      await fetch('https://www.google.com');
+
+      expect(testLog).toHaveBeenCalled();
+    });
+
+    it('logs to console if true', async () => {
+      const testLog = jest.fn();
+      const testConsoleLog = jest.fn();
+      console.log = testConsoleLog;
+      const fetch = fetchWithLog({ log: testLog });
+      await fetch('https://www.google.com', {}, true);
+
+      expect(testLog).toHaveBeenCalled();
+      expect(testConsoleLog).toHaveBeenCalledTimes(4);
+    });
+
+    it('logs to console if true with error', async () => {
+      const testLog = jest.fn();
+      const testConsoleLog = jest.fn();
+      console.log = testConsoleLog;
+      const fetch = fetchWithLog({ log: testLog });
+
+      try {
+        await fetch('www.google.com', {}, true);
+      } catch (ex) {
+        // do nothing
+      }
+
+      expect(testConsoleLog).toHaveBeenCalledTimes(1);
+    });
   });
 });
