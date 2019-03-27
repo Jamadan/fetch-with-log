864580c5206601e7b232ac6adc2cec212b129ff3
Jamie Hartley <jamie.hartley1@tesco.com>
2019-03-27 20:35:51 +0000

    remove comments

--- src/main/index.js
+++ src/main/index.js
@@ -26,39 +26,3 @@ export default args => {
     }
   };
 };
-// class FetchLog {
-
-//   constructor(args) {
-//     console.log("construct");
-//     console.log(args);
-
-//     this.log = args.log;
-//   }
-
-//   async fetch(url, options, consoleLog = false) {
-//     console.log("fetch");
-//     console.log(this.log);
-
-//     const startTime = Date.now();
-//     try {
-//       const res = await fetch(url, options);
-
-//       const endTime = Date.now();
-//       this.log({ url, options, res, time: endTime - startTime });
-//       if (consoleLog) {
-//         console.log(`url: ${url}`);
-//         console.log(`options: ${options}`);
-//         console.log(`res: ${res}`);
-//         console.log(`time: ${endTime - startTime}`);
-//       }
-//       return res;
-//     } catch (e) {
-//       if (consoleLog) {
-//         console.log("ERROR CAUGHT IN FETCH WITH LOG");
-//       }
-//       throw e;
-//     }
-//   }
-// }
-
-// export default FetchLog;
